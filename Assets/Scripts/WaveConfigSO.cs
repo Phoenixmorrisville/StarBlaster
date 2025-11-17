84d44102c6f30e40ea9ad6586072c6588cdbf891
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-11-16 20:23:15 -0500

    introducing Arrays

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaveConfig", menuName = "New WaveConfig")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float enemyMoveSpeed = 5f;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public float EnemyMoveSpeed()
+    {
+        return enemyMoveSpeed;
+    }
+
+    public Transform[] GetWaypoints()
+    {
+        Transform[] waypoints = new Transform[pathPrefab.childCount];
+
+        for (int i = 0; i < pathPrefab.childCount; i++)
+        {
+            waypoints[i] = pathPrefab.GetChild(i);
+        }
+
+        return waypoints;
+    }
+}
