4caa8927b65dba8dceb6e500bebd72fe2ad5cbc2
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-11-19 00:09:06 -0500

    Background Scrolling

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("Base Variables")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
-    [SerializeField] float fireRate = 0.2f;
+    [SerializeField] float baseFireRate = 0.2f;
 
+    [Header("AI Variables")]
     [SerializeField] bool useAI;
+    [SerializeField] float minimumFireRate = 0.2f;
+    [SerializeField] float fireRateVariance = 0f;
 
-    public bool isFiring;
+    [HideInInspector] public bool isFiring;
     Coroutine fireCoroutine;
 
     void Start()
@@ -43,12 +47,17 @@ public class Shooter : MonoBehaviour
         {
             GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
 
+            projectile.transform.rotation = transform.rotation;
+
             Rigidbody2D projectileRB = projectile.GetComponent<Rigidbody2D>();
-            projectileRB.linearVelocityY = projectileSpeed;
+            projectileRB.linearVelocity =  transform.up * projectileSpeed;
 
             Destroy(projectile, projectileLifetime);
 
-            yield return new WaitForSeconds(fireRate);
+            float waitTime = Random.Range(baseFireRate - fireRateVariance, baseFireRate + fireRateVariance);
+            waitTime = Mathf.Clamp(waitTime, minimumFireRate, float.MaxValue);
+
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
